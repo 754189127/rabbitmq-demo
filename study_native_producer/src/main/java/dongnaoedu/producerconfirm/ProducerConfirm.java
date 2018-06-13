9a2c1024d28b531f43a0f9b8dcd7b35455aee7b7
guowang <guowang@up360.com>
2018-06-13 14:43:59 +0800

    1

---
+++ study_native_producer/src/main/java/dongnaoedu/producerconfirm/ProducerConfirm.java
@@ -0,0 +1,22 @@
+package dongnaoedu.producerconfirm;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * 鍔ㄨ剳瀛﹂櫌-Mark鑰佸笀
+ * 鍒涘缓鏃ユ湡锛�2017/10/19
+ * 鍒涘缓鏃堕棿: 17:35
+ * 鍙戦�佹柟纭鍚屾妯″紡
+ */
+public class ProducerConfirm {
+
+    private final static String EXCHANGE_NAME = "producer_confirm";
+    private final static String ROUTE_KEY = "error";
+
+    public static void main(String[] args) throws IOException, TimeoutException,
+            InterruptedException {
+
+    }
+
+}
